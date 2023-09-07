c18e3be1f7e1d21d0506f52928b646686a8bd3f0
vader2k <76834964+vader2k@users.noreply.github.com>
2023-09-07 17:33:54 +0100

    started animations

--- architect_app/src/components/About.jsx
+++ architect_app/src/components/About.jsx
@@ -3,7 +3,11 @@ import { about1, about2, about3,right } from '../assets'
 import styles from '../style'
 import { about } from '../constants'
 
+
+
+
 const About = () => {
+
   return (
     <div className={` ${styles.boxWidth} items-center justify-center gap-10 flex md:flex-row flex-col-reverse py-5 `}>
       <div className='flex flex-row lg:flex-col gap-5 lg:gap-5 sm:gap-10 about-image  '>
@@ -19,7 +23,9 @@ const About = () => {
           </h1>
           {about.map((text,index)=>(
             <div key={text.id} className='text-[0.9rem] max-w-[600px] p-5'>
-              {text.content}
+                 <p>
+                {text.content}
+                </p>
               <button className='ab-btn flex items-center justify-center gap-6 w-[150px] h-[40px] bg-white mt-3 text-[0.8rem] border hover:border-black hover:border-1 hover:border-solid'>READ MORE <img className='w-[20px]' src={right}/></button>
             </div>
           ))}
@@ -29,5 +35,4 @@ const About = () => {
     </div>
   )
 }
-
 export default About
--- architect_app/src/pages/Home.jsx
+++ architect_app/src/pages/Home.jsx
@@ -1,27 +1,93 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import { useIntersection } from 'react-use'
+import { gsap } from 'gsap'
 import styles from '../style'
 import { Hero,About,Mission,Sample,Contacts} from '../components'
 
 
+
 const Home = () => {
+    const sectionAbout = useRef(null);
+    const intersection = useIntersection(sectionAbout , {
+        root: null,
+        rootMargin: '100px',
+        threshold: 0.5,
+    });
+
+
+    const fadeIn = element => {
+        gsap.to(element, 1, {
+            opacity:1,
+            y:-60,
+            ease: 'power4.out',
+            stagger:{
+                amount:.3,
+            }
+        })
+    };
+
+    const fadeOut = element => {
+        gsap.to(element, 1, {
+            opacity:0,
+            y:-20,
+            ease: 'power4.out'
+        })
+    };
+
+    intersection && intersection.intersectionRatio < 0.5 ?
+    fadeOut('.fadeOut') : fadeIn('.fadeIn');
+
+
+
+
+
+    const sectionSample = useRef(null);
+    const intersect = useIntersection(sectionSample, {
+        root: null,
+        rootMargin: '100px',
+        threshold: 0.5,
+    });
+
+    const fadesIn = item => {
+        gsap.to(item, 1, {
+            opacity: 1,
+            y: -60,
+            ease: 'power4.out',
+            stagger: {
+                amount: .3,
+            }
+        })
+    }
+    const fadesOut = item => {
+        gsap.to(item, 1, {
+            opacity: 0,
+            y: -20,
+            ease: 'power4.out',
+        })
+    }
+ 
+    intersect && intersect.intersectionRatio < 0.5 ?
+    fadesOut('.fadesOut') : fadesIn('.fadesIn');
+
+
   return (
     <>
             <div className={`${styles.paddingX} ${styles.flexCenter}`}>
                 <Hero/>
             </div>
 
-            <div className={`${styles.paddingX} ${styles.flexCenter} ab my-10`}>
+            <div ref={sectionAbout} className={`${styles.paddingX} ${styles.flexCenter} ab my-10 fadeIn fadeOut`}>
                 <About/>
             </div>
 
-            <div  className={`${styles.paddingX} ${styles.flexCenter}`}>
+            <div  className={`${styles.paddingX} ${styles.flexCenter} `}>
             <div className={`${styles.boxWidth}`}>
                 <Mission/>
             </div>
             </div>
 
-            <div  className={`${styles.paddingX} ${styles.flexCenter}`}>
-            <div className={`${styles.boxWidth}`}>
+            <div ref={sectionSample} className={`${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth} fadesIn fadesOut`}>
                 <Sample/>
             </div>
             </div>
